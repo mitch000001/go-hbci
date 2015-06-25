82a40fd72f99f2cad520a346ced1cfb77c40385c
Michael Wagner <mitch.wagna@gmail.com>
2015-06-25 12:10:29 +0200

    Add dialog

--- dialog.go
+++ dialog.go
@@ -1,5 +1,332 @@
 package hbci
 
+import (
+	"bufio"
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const initialDialogID = "0"
+const initialClientSystemID = "0"
+const anonymousClientID = "9999999999"
+
+type Dialog interface {
+	Init() (string, error)
+}
+
+func newDialog(bankId BankId, hbciUrl string, clientId string, signatureProvider SignatureProvider, encryptionProvider EncryptionProvider) *dialog {
+	return &dialog{
+		hbciUrl:            hbciUrl,
+		BankID:             bankId,
+		ClientID:           clientId,
+		signatureProvider:  signatureProvider,
+		encryptionProvider: encryptionProvider,
+	}
+}
+
+type dialog struct {
+	hbciUrl            string
+	BankID             BankId
+	ClientID           string
+	ClientSystemID     string
+	messageCount       int
+	signatureProvider  SignatureProvider
+	encryptionProvider EncryptionProvider
+}
+
+func (d *dialog) nextMessageNumber() int {
+	d.messageCount += 1
+	return d.messageCount
+}
+
+func (d *dialog) dialogEnd(dialogId string) *DialogFinishingMessage {
+	dialogEnd := new(DialogFinishingMessage)
+	messageNum := d.nextMessageNumber()
+	dialogEnd.basicClientMessage = newBasicClientMessage(dialogEnd)
+	dialogEnd.Header = NewMessageHeaderSegment(-1, 220, initialDialogID, messageNum)
+	dialogEnd.End = NewMessageEndSegment(8, messageNum)
+	dialogEnd.DialogEnd = NewDialogEndSegment(dialogId)
+	return dialogEnd
+}
+
+func (d *dialog) post(message []byte) ([]byte, error) {
+	fmt.Printf("Marshaled message: %q\n", strings.Split(string(message), "'"))
+	encodedMessage := base64.StdEncoding.EncodeToString(message)
+	response, err := http.Post(d.hbciUrl, "application/vnd.hbci", strings.NewReader(encodedMessage))
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+	if response.StatusCode == http.StatusOK {
+		decodedReader := base64.NewDecoder(base64.StdEncoding, response.Body)
+		bodyBytes, err := ioutil.ReadAll(decodedReader)
+		if err != nil {
+			return nil, err
+		}
+		return bodyBytes, nil
+	} else {
+		bodyBytes, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			return nil, err
+		}
+		return bodyBytes, nil
+	}
+}
+
+func (d *dialog) dial(message []byte) ([]byte, error) {
+	conn, err := net.Dial("tcp4", d.hbciUrl)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+	conn.SetReadDeadline(time.Now().Add(15 * time.Second))
+	fmt.Fprintf(conn, "%s\r\n\r\n", string(message))
+	buf := bufio.NewReader(conn)
+	// read answer header
+	header, err := buf.ReadString('\'')
+	if err != nil {
+		return nil, err
+	}
+	headerItems := strings.Split(header, "+")
+	if len(headerItems) < 2 {
+		return nil, fmt.Errorf("Response header too short")
+	}
+	sizeString := headerItems[1]
+	size, err := strconv.Atoi(sizeString)
+	if err != nil {
+		return nil, fmt.Errorf("Error while parsing message size: %T:%v\n")
+	}
+	messageBuf := make([]byte, size)
+	buf.Read(messageBuf)
+	var retBuf bytes.Buffer
+	retBuf.WriteString(header)
+	retBuf.Write(messageBuf)
+	return retBuf.Bytes(), err
+}
+
+func NewPinTanDialog(bankId BankId, hbciUrl string, clientId string) *pinTanDialog {
+	signatureProvider := NewPinTanSignatureProvider(nil)
+	encryptionProvider := NewPinTanEncryptionProvider(nil, "")
+	d := &pinTanDialog{
+		dialog: newDialog(bankId, hbciUrl, clientId, signatureProvider, encryptionProvider),
+	}
+	return d
+}
+
+type pinTanDialog struct {
+	*dialog
+	pin           string
+	signingKey    Key
+	pinTanKeyName KeyName
+}
+
+func (d *pinTanDialog) SetPin(pin string) {
+	d.pin = pin
+	pinKey := NewPinKey(pin, NewPinTanKeyName(d.BankID, d.ClientID, "S"))
+	d.signingKey = pinKey
+	d.signatureProvider = NewPinTanSignatureProvider(pinKey)
+	d.encryptionProvider = NewPinTanEncryptionProvider(pinKey, d.ClientSystemID)
+}
+
+func (d *pinTanDialog) Init() (string, error) {
+	initMessage := NewDialogInitializationClientMessage()
+	messageNum := d.nextMessageNumber()
+	initMessage.Header = NewMessageHeaderSegment(-1, 220, initialDialogID, messageNum)
+	initMessage.End = NewMessageEndSegment(8, messageNum)
+	initMessage.Identification = NewIdentificationSegment(d.BankID, d.ClientID, initialClientSystemID, true)
+	initMessage.ProcessingPreparation = NewProcessingPreparationSegment(0, 0, 1)
+	controlRef := "1234567890"
+	initMessage.SignatureBegin = d.signatureProvider.NewSignatureHeader(controlRef, 0, "0")
+	initMessage.SignatureEnd = NewSignatureEndSegment(7, controlRef)
+	initMessage.SetNumbers()
+	err := initMessage.Sign(d.signatureProvider)
+	if err != nil {
+		return "", err
+	}
+	initMessage.SetSize()
+	encryptedInitMessage, err := initMessage.Encrypt(d.encryptionProvider)
+	if err != nil {
+		return "", err
+	}
+	marshaledMessage, err := encryptedInitMessage.MarshalHBCI()
+	if err != nil {
+		return "", err
+	}
+
+	response, err := d.post(marshaledMessage)
+	if err != nil && err != io.EOF {
+		return "", err
+	}
+
+	fmt.Printf("Response: %q\n", strings.Split(string(response), "'"))
+
+	dialogEnd := d.dialogEnd(initialDialogID)
+	dialogEnd.SignatureBegin = d.signatureProvider.NewSignatureHeader(controlRef, 0, "0")
+	dialogEnd.SignatureEnd = NewSignatureEndSegment(7, controlRef)
+	dialogEnd.SetNumbers()
+	err = dialogEnd.Sign(d.signatureProvider)
+	if err != nil {
+		return "", err
+	}
+	dialogEnd.SetSize()
+	encryptedDialogEnd, err := dialogEnd.Encrypt(d.encryptionProvider)
+	if err != nil {
+		return "", err
+	}
+	marshaledEndMessage, err := encryptedDialogEnd.MarshalHBCI()
+	if err != nil {
+		return "", err
+	}
+	response, err = d.post(marshaledEndMessage)
+	if err != nil && err != io.EOF {
+		return "", err
+	}
+
+	return string(response), nil
+}
+
+func (d *pinTanDialog) SyncClientSystemID() (string, error) {
+	syncMessage := new(SynchronisationMessage)
+	messageNum := d.nextMessageNumber()
+	syncMessage.basicClientMessage = newBasicClientMessage(syncMessage)
+	syncMessage.Header = NewMessageHeaderSegment(-1, 300, initialDialogID, messageNum)
+	syncMessage.End = NewMessageEndSegment(8, messageNum)
+	syncMessage.Identification = NewIdentificationSegment(d.BankID, d.ClientID, initialClientSystemID, true)
+	syncMessage.ProcessingPreparation = NewProcessingPreparationSegment(0, 0, 1)
+	syncMessage.Sync = NewSynchronisationSegment(0)
+	controlRef := "1"
+	syncMessage.SignatureBegin = d.signatureProvider.NewSignatureHeader(controlRef, 0, "0")
+	syncMessage.SignatureEnd = NewSignatureEndSegment(7, controlRef)
+	syncMessage.SetNumbers()
+	err := syncMessage.Sign(d.signatureProvider)
+	if err != nil {
+		return "", err
+	}
+	syncMessage.SetSize()
+	encryptedSyncMessage, err := syncMessage.Encrypt(d.encryptionProvider)
+	if err != nil {
+		return "", err
+	}
+	marshaledMessage, err := encryptedSyncMessage.MarshalHBCI()
+	if err != nil {
+		return "", err
+	}
+
+	response, err := d.post(marshaledMessage)
+	if err != nil && err != io.EOF {
+		return "", err
+	}
+	fmt.Printf("Response: %q\n", strings.Split(string(response), "'"))
+
+	dialogEnd := d.dialogEnd(initialDialogID)
+	dialogEnd.SignatureBegin = d.signatureProvider.NewSignatureHeader(controlRef, 0, "0")
+	dialogEnd.SignatureEnd = NewSignatureEndSegment(7, controlRef)
+	dialogEnd.SetNumbers()
+	err = dialogEnd.Sign(d.signatureProvider)
+	if err != nil {
+		return "", err
+	}
+	dialogEnd.SetSize()
+	marshaledEndMessage, err := dialogEnd.MarshalHBCI()
+	if err != nil {
+		return "", err
+	}
+	response, err = d.post(marshaledEndMessage)
+	if err != nil && err != io.EOF {
+		return "", err
+	}
+
+	return string(response), nil
+}
+
+func (d *pinTanDialog) CommunicationAccess() (string, error) {
+	comm := NewCommunicationAccessMessage(d.BankID, d.BankID, 5, "")
+	comm.Header = NewMessageHeaderSegment(0, 220, initialDialogID, 1)
+	comm.End = NewMessageEndSegment(3, 1)
+	comm.SetSize()
+	marshaled, err := comm.MarshalHBCI()
+	if err != nil {
+		return "", err
+	}
+	fmt.Printf("Marshaled: %q\n", string(marshaled))
+	response, err := d.post(marshaled)
+	if err != nil && err != io.EOF {
+		return "", err
+	}
+	return string(response), nil
+}
+
+func (d *pinTanDialog) Anonymous(fn func() (string, error)) (string, error) {
+	initMessage := NewDialogInitializationClientMessage()
+	messageNum := d.nextMessageNumber()
+	initMessage.Header = NewMessageHeaderSegment(-1, 220, initialDialogID, messageNum)
+	initMessage.End = NewMessageEndSegment(8, messageNum)
+	initMessage.Identification = NewIdentificationSegment(d.BankID, d.ClientID, "0", false)
+	initMessage.ProcessingPreparation = NewProcessingPreparationSegment(0, 0, 1)
+	initMessage.SetNumbers()
+	initMessage.SetSize()
+	marshaledMessage, err := initMessage.MarshalHBCI()
+	if err != nil {
+		return "", err
+	}
+
+	response, err := d.post(marshaledMessage)
+	if err != nil && err != io.EOF {
+		return "", err
+	}
+
+	fmt.Printf("Response: %q\n", strings.Split(string(response), "'"))
+
+	res, err := fn()
+	if err != nil && err != io.EOF {
+		return "", err
+	}
+	fmt.Printf("Response: %q\n", strings.Split(res, "'"))
+
+	dialogEnd := d.dialogEnd("0")
+	dialogEnd.SetNumbers()
+	dialogEnd.SetSize()
+	marshaledEndMessage, err := dialogEnd.MarshalHBCI()
+	if err != nil {
+		return "", err
+	}
+	response, err = d.post(marshaledEndMessage)
+	if err != nil && err != io.EOF {
+		return "", err
+	}
+
+	return string(response), nil
+}
+
+func NewRDHDialog(bankId BankId, hbciUrl string, clientId string) *rdhDialog {
+	key, err := GenerateSigningKey()
+	if err != nil {
+		panic(err)
+	}
+	signingKey := NewRSAKey(key, NewInitialKeyName(bankId.CountryCode, bankId.ID, clientId, "S"))
+	provider := NewRDHSignatureProvider(signingKey)
+	d := &rdhDialog{
+		dialog:      newDialog(bankId, hbciUrl, clientId, provider, nil),
+		SigningKey:  signingKey,
+		SignatureID: 12345,
+	}
+	return d
+}
+
+type rdhDialog struct {
+	*dialog
+	SignatureID int
+	SigningKey  Key
+}
+
 func NewDialogInitializationClientMessage() *DialogInitializationClientMessage {
 	d := &DialogInitializationClientMessage{}
 	d.basicClientMessage = newBasicClientMessage(d)
