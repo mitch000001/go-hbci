8cc600dccfadd8079b9f4d048a97887c25872a84
Michael Wagner <mitch.wagna@gmail.com>
2015-07-20 22:53:16 +0200

    Add element extractor

---
+++ element/element_extractor.go
@@ -0,0 +1,38 @@
+package element
+
+import (
+	"fmt"
+
+	"github.com/mitch000001/go-hbci/token"
+)
+
+func NewElementExtractor(segment []byte) *ElementExtractor {
+	return &ElementExtractor{
+		rawSegment: segment,
+	}
+}
+
+type ElementExtractor struct {
+	rawSegment []byte
+	elements   [][]byte
+}
+
+func (e *ElementExtractor) Extract() ([][]byte, error) {
+	var current string
+	lexer := token.NewStringLexer("ElementExtractor", string(e.rawSegment))
+	for lexer.HasNext() {
+		t := lexer.Next()
+		if t.Type() == token.ERROR {
+			return nil, fmt.Errorf("%T: SyntaxError at position %d: %q\n(%q)", e, t.Pos(), t.Value(), e.rawSegment)
+		}
+		if t.Type() == token.SEGMENT_END_MARKER || t.Type() == token.DATA_ELEMENT_SEPARATOR {
+			e.elements = append(e.elements, []byte(current))
+			current = ""
+		} else {
+			current += t.Value()
+		}
+	}
+	result := make([][]byte, len(e.elements))
+	copy(result, e.elements)
+	return result, nil
+}
---
+++ element/element_extractor_test.go
@@ -0,0 +1,64 @@
+package element
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestElementExtractorExtract(t *testing.T) {
+	type testData struct {
+		in  string
+		out []string
+		err error
+	}
+	tests := []testData{
+		{
+			"abcde+123+012+de+'",
+			[]string{
+				"abcde",
+				"123",
+				"012",
+				"de",
+				"",
+			},
+			nil,
+		},
+		{
+			"abcde:123:012+de+'",
+			[]string{
+				"abcde:123:012",
+				"de",
+				"",
+			},
+			nil,
+		},
+	}
+
+	for _, test := range tests {
+		extractor := NewElementExtractor([]byte(test.in))
+
+		extracted, err := extractor.Extract()
+
+		if err != nil {
+			t.Logf("Expected no error, got %T:%v\n", err, err)
+			t.Fail()
+		}
+
+		if extracted == nil {
+			t.Logf("Expected result not to be nil\n")
+			t.Fail()
+		}
+
+		actual := make([]string, len(extracted))
+		for i, b := range extracted {
+			actual[i] = string(b)
+		}
+
+		if !reflect.DeepEqual(test.out, actual) {
+			t.Logf("Extract: \n%q\n", extracted)
+			t.Logf("Expected result to equal\n%q\n\tgot\n%q\n", test.out, actual)
+			t.Fail()
+		}
+
+	}
+}
