3a55f8594575d2c299b886e42a794f7858673e2d
Michael Wagner <mitch.wagna@gmail.com>
2015-08-12 16:11:10 +0200

    Resolve merge errors

--- element/data_element.go
+++ element/data_element.go
@@ -36,108 +36,6 @@ type GroupDataElementGroup interface {
 	Elements() []DataElement
 }
 
-type DataElementType int
-
-const (
-	// DataElements
-	AlphaNumericDE DataElementType = iota + 1
-	TextDE
-	NumberDE
-	DigitDE
-	FloatDE
-	DTAUSCharsetDE
-	BinaryDE
-	// Derived types
-	BooleanDE
-	CodeDE
-	DateDE
-	VirtualDateDE
-	TimeDE
-	IdentificationDE
-	CountryCodeDE
-	CurrencyDE
-	ValueDE
-	// Multiple used element
-	AmountGDEG
-	BankIdentificationGDEG
-	AccountConnectionGDEG
-	BalanceGDEG
-	AddressGDEG
-	SecurityMethodVersionGDEG
-	AcknowlegdementParamsGDEG
-	// DataElementGroups
-	SegmentHeaderDEG
-	ReferenceMessageDEG
-	AcknowledgementDEG
-	SecurityIdentificationDEG
-	SecurityDateDEG
-	HashAlgorithmDEG
-	SignatureAlgorithmDEG
-	EncryptionAlgorithmDEG
-	KeyNameDEG
-	CertificateDEG
-	PublicKeyDEG
-	SupportedLanguagesDEG
-	SupportedHBCIVersionDEG
-	CommunicationParameterDEG
-	SupportedSecurityMethodDEG
-	PinTanDEG
-	SecurityProfileDEG
-)
-
-var typeName = map[DataElementType]string{
-	AlphaNumericDE: "an",
-	TextDE:         "txt",
-	NumberDE:       "num",
-	DigitDE:        "dig",
-	FloatDE:        "float",
-	DTAUSCharsetDE: "dta",
-	BinaryDE:       "bin",
-	// Derived types
-	BooleanDE:        "jn",
-	CodeDE:           "code",
-	DateDE:           "dat",
-	VirtualDateDE:    "vdat",
-	TimeDE:           "tim",
-	IdentificationDE: "id",
-	CountryCodeDE:    "ctr",
-	CurrencyDE:       "cur",
-	ValueDE:          "wrt",
-	// Multiple used element
-	AmountGDEG:                "btg",
-	BankIdentificationGDEG:    "kik",
-	AccountConnectionGDEG:     "ktv",
-	BalanceGDEG:               "sdo",
-	AddressGDEG:               "addr",
-	SecurityMethodVersionGDEG: "Unterstützte Sicherheitsverfahren",
-	AcknowlegdementParamsGDEG: "Rückmeldungsparameter",
-	// DataElementGroups
-	SegmentHeaderDEG:          "Segmentkopf",
-	ReferenceMessageDEG:       "Bezugsnachricht",
-	AcknowledgementDEG:        "Rückmeldung",
-	SecurityIdentificationDEG: "Sicherheitsidentifikation, Details",
-	SecurityDateDEG:           "Sicherheitsdatum und -uhrzeit",
-	HashAlgorithmDEG:          "Hashalgorithmus",
-	SignatureAlgorithmDEG:     "Signaturalgorithmus",
-	EncryptionAlgorithmDEG:    "Verschlüsselungsalgorithmus",
-	KeyNameDEG:                "Schlüsselname",
-	CertificateDEG:            "Zertifikat",
-	PublicKeyDEG:              "Öffentlicher Schlüssel",
-	SupportedLanguagesDEG:     "Unterstützte Sprachen",
-	SupportedHBCIVersionDEG:   "Unterstützte HBCI-Versionen",
-	CommunicationParameterDEG: "Kommunikationsparameter",
-	PinTanDEG:                 "PIN-TAN",
-	SecurityProfileDEG:        "Sicherheitsprofil",
-}
-
-func (d DataElementType) String() string {
-	s := typeName[d]
-	if s == "" {
-		return fmt.Sprintf("DataElementType%d", int(d))
-	}
-	return s
-}
-
 func New(typ DataElementType, value interface{}, maxLength int) DataElement {
 	return &dataElement{value, typ, maxLength, false}
 }
@@ -559,7 +457,7 @@ func (b *BooleanDataElement) UnmarshalHBCI(value []byte) error {
 func NewCodeDataElement(val string, maxLength int, validSet []string) *CodeDataElement {
 	sort.Strings(validSet)
 	return &CodeDataElement{
-		AlphaNumericDataElement: NewAlphaNumericDataElement(val, maxLength),
+		AlphaNumericDataElement: NewAlphaNumeric(val, maxLength),
 		validSet:                validSet,
 	}
 }
