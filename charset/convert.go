aefdef8830c2cd6e552eb5edff80d42908c4ab9e
Michael Wagner <mitch.wagna@gmail.com>
2017-01-02 16:37:55 +0100

    chore(charset): remove unused writer

--- charset/convert.go
+++ charset/convert.go
@@ -1,11 +1,6 @@
 package charset
 
-import (
-	"bytes"
-	"io"
-
-	"golang.org/x/text/encoding/charmap"
-)
+import "golang.org/x/text/encoding/charmap"
 
 func mustConvert(b []byte, err error) []byte {
 	if err != nil {
@@ -38,18 +33,4 @@ func ToISO8859_1(utf8String string) []byte {
 	encoder := charmap.ISO8859_1.NewEncoder()
 	// TODO: propagate errors
 	return mustConvert(encoder.Bytes([]byte(utf8String)))
-}
-
-func NewISO8859_1Writer() io.Writer {
-	var b bytes.Buffer
-	return &iso8859_1Writer{&b}
-}
-
-type iso8859_1Writer struct {
-	*bytes.Buffer
-}
-
-func (i *iso8859_1Writer) Write(p []byte) (int, error) {
-	iso8859_1Bytes := ToISO8859_1(string(p))
-	return i.Buffer.Write(iso8859_1Bytes)
 }
