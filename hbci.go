a3b7c3e583eddeeb3fe8210932e7f4e99bf5931e
Michael Wagner <mitch.wagna@gmail.com>
2018-03-26 15:09:08 +0200

    chore: cleanup toplevel package

--- hbci.go
+++ hbci.go
@@ -2,27 +2,15 @@ package hbci
 
 import "github.com/mitch000001/go-hbci/internal"
 
+// Version represents the current library version
 const Version = "0.1.3"
 
+// SetDebugMode enables or disables logging on the debug logger
 func SetDebugMode(debug bool) {
 	internal.SetDebugMode(debug)
 }
 
+// SetInfoLog enables or disables logging on the info logger
 func SetInfoLog(info bool) {
 	internal.SetInfoLog(info)
-}
-
-type Marshaler interface {
-	MarshalHBCI() ([]byte, error)
-}
-
-type Unmarshaler interface {
-	UnmarshalHBCI([]byte) error
-}
-
-// supportedUnmarshaler maps segment IDs to Unmarshalers
-var supportedUnmarshaler = map[string]Unmarshaler{}
-
-func MakeCall() string {
-	return ""
 }
