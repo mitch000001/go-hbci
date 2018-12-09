14f5f165ca0510c3b5a8efe24bf4e4a042d26beb
Michael Wagner <mitch.wagna@gmail.com>
2018-12-09 11:32:15 +0100

    chore: version bump

--- hbci.go
+++ hbci.go
@@ -3,7 +3,7 @@ package hbci
 import "github.com/mitch000001/go-hbci/internal"
 
 // Version represents the current library version
-const Version = "0.1.5"
+const Version = "0.2.0"
 
 // SetDebugMode enables or disables logging on the debug logger
 func SetDebugMode(debug bool) {
