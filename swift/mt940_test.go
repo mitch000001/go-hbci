d826d4e9c0d6bb57ba624d33a49cd8c16eae9c59
jeppsson <sven.jeppsson@red6-es.de>
2019-02-03 11:07:32 +0100

    chore: make a tests better readable

--- swift/mt940_test.go
+++ swift/mt940_test.go
@@ -2,8 +2,6 @@ package swift
 
 import (
 	"reflect"
-	"strconv"
-	"strings"
 	"testing"
 	"time"
 
@@ -154,101 +152,64 @@ func TestTransactionTagUnmarshal(t *testing.T) {
 	}
 }
 
-func TestTransactionTagOrder(t *testing.T) {
-	testdata := "\r\n:20:HBCIKTOLST"
-	for i := 0; i < 10; i++ {
-		testdata += "\r\n:25:12345678/1234123456" +
-			"\r\n:28C:0" +
-			"\r\n:60F:C181105EUR1234,56" +
-			"\r\n:61:1811051105DR50,NMSCNONREF" +
-			"\r\n/OCMT/EUR50,//CHGS/   0,/" +
-			"\r\n:86:177?00SB-SEPA-Ueberweisung?20" + strconv.Itoa(i+10) + "                                                                                                                                                 ?30?31?32Max Meier                  ?33                           ?34000" +
-			"\r\n:62F:C190125EUR1234,56"
+func TestBookingDateBug(t *testing.T) {
+	tests := []struct {
+		testdata            string
+		expectedBookingDate string
+	}{
+		{
+			// Valuta Date 2019-01-01 Booking Date 2018-12-28
+			testdata: "\r\n:20:HBCIKTOLST" + "\r\n:25:12345678/1234123456" +
+				"\r\n:28C:0" +
+				"\r\n:60F:C181228EUR1234,56" +
+				"\r\n:61:1901011228DR50,NMSCNONREF" +
+				"\r\n/OCMT/EUR50,//CHGS/   0,/" +
+				"\r\n:86:177?00SB-SEPA-Ueberweisung?20                                                                                                                                                     ?30?31?32Max Maier                  ?33                           ?34000" +
+				"\r\n:62F:C190125EUR1234,56" +
+				"\r\n-",
+			expectedBookingDate: "2018-12-28",
+		},
+		// Valuta Date 2019-01-01 Booking Date 2019-01-01
+		{
+			testdata: "\r\n:20:HBCIKTOLST" + "\r\n:25:12345678/1234123456" +
+				"\r\n:28C:0" +
+				"\r\n:60F:C181228EUR1234,56" +
+				"\r\n:61:1901011228DR50,NMSCNONREF" +
+				"\r\n/OCMT/EUR50,//CHGS/   0,/" +
+				"\r\n:86:177?00SB-SEPA-Ueberweisung?20                                                                                                                                                     ?30?31?32Max Maier                  ?33                           ?34000" +
+				"\r\n:62F:C190125EUR1234,56" +
+				"\r\n-",
+			expectedBookingDate: "2018-12-28",
+		}, {
+			// Valuta Date 2018-12-28 Booking Date 2019-01-01 (not sure if that can happen ? )
 
+			testdata: "\r\n:20:HBCIKTOLST" + "\r\n:25:12345678/1234123456" +
+				"\r\n:28C:0" +
+				"\r\n:60F:C181228EUR1234,56" +
+				"\r\n:61:1812280101DR50,NMSCNONREF" +
+				"\r\n/OCMT/EUR50,//CHGS/   0,/" +
+				"\r\n:86:177?00SB-SEPA-Ueberweisung?20                                                                                                                                                     ?30?31?32Max Maier                  ?33                           ?34000" +
+				"\r\n:62F:C190125EUR1234,56" +
+				"\r\n-",
+			expectedBookingDate: "2019-01-01",
+		},
 	}
-	testdata += "\r\n-"
-	mt := &MT940{}
-	mt.Unmarshal([]byte(testdata))
-	for i, tr := range mt.Transactions {
-		if strings.TrimSpace(tr.Description.Purpose[0]) != strconv.Itoa(i+10) {
-			t.Logf("Purpose at index %d should be %d but is %s", i, i+10, tr.Description.Purpose[0])
+
+	for _, test := range tests {
+		mt := &MT940{}
+
+		err := mt.Unmarshal([]byte(test.testdata))
+		if err != nil {
+			t.Log(err)
 			t.Fail()
 		}
+		if len(mt.Transactions) != 1 {
+			t.Log("There should be exactly one transaction")
+		}
 
-	}
+		if test.expectedBookingDate != mt.Transactions[0].Transaction.BookingDate.String() {
 
-}
-
-func TestTransactionListWithUnvalidData(t *testing.T) {
-	testdata := "\r\n:20:HBCIKTOLST"
-	testdata += "\r\n:25:12345678/1234123456" +
-		"\r\n:28C:0" +
-		"\r\n:60F:C181105EUR1234,56" +
-		"\r\n:86:177?00SB-SEPA-Ueberweisung?20                                                                                                                                                   ?30?31?32Max Meier                  ?33                           ?34000" +
-		"\r\n:62F:C190125EUR1234,56"
-
-	testdata += "\r\n-"
-	mt := &MT940{}
-	error := mt.Unmarshal([]byte(testdata))
-	if error == nil {
-		t.Log("Error expected because of CustomTag without TransactionTag")
-		t.Fail()
-	}
-
-}
-
-func TestTransactionWithRedefineCustomDataTag(t *testing.T) {
-	testdata := "\r\n:20:HBCIKTOLST"
-	testdata += "\r\n:25:12345678/1234123456" +
-		"\r\n:28C:0" +
-		"\r\n:60F:C181105EUR1234,56" +
-
-		"\r\n:61:1811051105DR50,NMSCNONREF" +
-		"\r\n:86:177?00SB-SEPA-Ueberweisung?20                                                                                                                                                   ?30?31?32Max Meier                  ?33                           ?34000" +
-		"\r\n:86:177?00SB-SEPA-Ueberweisung?20                                                                                                                                                   ?30?31?32Max Meier                  ?33                           ?34000" +
-		"\r\n:62F:C190125EUR1234,56"
-
-	testdata += "\r\n-"
-	mt := &MT940{}
-	error := mt.Unmarshal([]byte(testdata))
-	if error == nil {
-		t.Log("Error expected because of more than CustomTag after TransactionTag")
-		t.Fail()
-	}
-}
-
-func TestBookingDateBug(t *testing.T) {
-	testdata := "\r\n:20:HBCIKTOLST"
-	// Valuta Date 2019-01-01 Booking Date 2018-12-28
-	testdata += "\r\n:25:12345678/1234123456" +
-		"\r\n:28C:0" +
-		"\r\n:60F:C181228EUR1234,56" +
-		"\r\n:61:1901011228DR50,NMSCNONREF" +
-		"\r\n/OCMT/EUR50,//CHGS/   0,/" +
-		"\r\n:86:177?00SB-SEPA-Ueberweisung?202018-12-28                                                                                                                                           ?30?31?32Max Maier                  ?33                           ?34000" +
-		"\r\n:62F:C190125EUR1234,56"
-	// Valuta Date 2019-01-01 Booking Date 2019-01-01
-	testdata += "\r\n:25:12345678/1234123456" +
-		"\r\n:28C:0" +
-		"\r\n:60F:C181228EUR1234,56" +
-		"\r\n:61:1901010101DR50,NMSCNONREF" +
-		"\r\n/OCMT/EUR50,//CHGS/   0,/" +
-		"\r\n:86:177?00SB-SEPA-Ueberweisung?202019-01-01                                                                                                                                           ?30?31?32Max Meier                  ?33                           ?34000" +
-		"\r\n:62F:C190125EUR1234,56"
-	// Valuta Date 2018-12-28 Booking Date 2019-01-01 (not sure if that can happen ? )
-	testdata += "\r\n:25:12345678/1234123456" +
-		"\r\n:28C:0" +
-		"\r\n:60F:C181228EUR1234,56" +
-		"\r\n:61:1812280101DR50,NMSCNONREF" +
-		"\r\n/OCMT/EUR50,//CHGS/   0,/" +
-		"\r\n:86:177?00SB-SEPA-Ueberweisung?202019-01-01                                                                                                                                           ?30?31?32Max Meier                  ?33                           ?34000" +
-		"\r\n:62F:C190125EUR1234,56"
-	testdata += "\r\n-"
-	mt := &MT940{}
-	mt.Unmarshal([]byte(testdata))
-	for _, tr := range mt.Transactions {
-		if strings.TrimSpace(tr.Description.Purpose[0]) != tr.Transaction.BookingDate.String() {
-			t.Logf("Booking date should be %s but is %s", strings.TrimSpace(tr.Description.Purpose[0]), tr.Transaction.BookingDate.String())
+			t.Logf("Booking date should be %s but is %s", test.expectedBookingDate, mt.Transactions[0].Transaction.BookingDate.String())
 			t.Fail()
 		}
 
