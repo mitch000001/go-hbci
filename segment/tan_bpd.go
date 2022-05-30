3d0ca03b101b072b9e1c6a8f526641be868b2f05
Michael Wagner <mitch.wagna@gmail.com>
2022-05-30 12:08:20 +0200

    feat(segment): add HITANS:7

--- segment/tan_bpd.go
+++ segment/tan_bpd.go
@@ -12,7 +12,7 @@ type TanBankParameter interface {
 	TanProcessParameters() []domain.TanProcessParameter
 }
 
-//go:generate go run ../cmd/unmarshaler/unmarshaler_generator.go -segment TanBankParameterSegment -segment_interface TanBankParameter -segment_versions="TanBankParameterV6:6:Segment"
+//go:generate go run ../cmd/unmarshaler/unmarshaler_generator.go -segment TanBankParameterSegment -segment_interface TanBankParameter -segment_versions="TanBankParameterV6:6:Segment,TanBankParameterV7:7:Segment"
 
 type TanBankParameterSegment struct {
 	TanBankParameter
@@ -63,6 +63,51 @@ func (t *TanBankParameterV6) TanProcessParameters() []domain.TanProcessParameter
 			TanProcessTechnicalID: pp.TechnicalIDTanProcess.Val(),
 			TanProcessName:        pp.ZKATanProcess.Val(),
 			TanProcessVersion:     pp.ZKATanProcessVersion.Val(),
+			TwoStepProcessName:    pp.TwoStepProcessName.Val(),
+		})
+	}
+	return processParams
+}
+
+// TanBankParameterV7
+//
+// Zwei-Schritt-TAN-Einreichung, Parameter
+type TanBankParameterV7 struct {
+	Segment                     `yaml:"-"`
+	MaxJobs                     *element.NumberDataElement             `yaml:"MaxJobs"`
+	MinSignatures               *element.NumberDataElement             `yaml:"MinSignatures"`
+	SecurityClass               *element.CodeDataElement               `yaml:"SecurityClass"`
+	Tan2StepSubmissionParameter *element.Tan2StepSubmissionParameterV7 `yaml:"Tan2StepSubmissionParameter"`
+}
+
+func (t *TanBankParameterV7) Version() int         { return 7 }
+func (t *TanBankParameterV7) ID() string           { return TanBankParameterID }
+func (t *TanBankParameterV7) referencedId() string { return ProcessingPreparationID }
+func (t *TanBankParameterV7) sender() string       { return senderBank }
+
+func (t *TanBankParameterV7) elements() []element.DataElement {
+	return []element.DataElement{
+		t.MaxJobs,
+		t.MinSignatures,
+		t.SecurityClass,
+		t.Tan2StepSubmissionParameter,
+	}
+}
+
+func (t *TanBankParameterV7) TanProcessParameters() []domain.TanProcessParameter {
+	var processParams []domain.TanProcessParameter
+	for _, de := range t.Tan2StepSubmissionParameter.ProcessParameters.GroupDataElements() {
+		pp, ok := de.(*element.Tan2StepSubmissionProcessParameterV7)
+		if !ok {
+			continue
+		}
+		processParams = append(processParams, domain.TanProcessParameter{
+			Steps:                 2,
+			SecurityFunction:      pp.SecurityFunction.Val(),
+			TanProcess:            pp.TanProcess.Val(),
+			TanProcessTechnicalID: pp.TechnicalIDTanProcess.Val(),
+			TanProcessName:        pp.DKTanProcess.Val(),
+			TanProcessVersion:     pp.DKTanProcessVersion.Val(),
 			TwoStepProcessName:    pp.TwoStepProcessName.Val(),
 		})
 	}
