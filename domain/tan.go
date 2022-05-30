5ce8a26162eb50cd6da88add3b62ff6f9bb10b9d
Michael Wagner <mitch.wagna@gmail.com>
2022-05-30 12:51:03 +0200

    chore(tan): pretty print supported tan processes

---
+++ domain/tan.go
@@ -0,0 +1,11 @@
+package domain
+
+type TanProcessParameter struct {
+	Steps                 int
+	SecurityFunction      string
+	TanProcess            string
+	TanProcessTechnicalID string
+	TanProcessName        string
+	TanProcessVersion     string
+	TwoStepProcessName    string
+}
--- segment/tan_bpd.go
+++ segment/tan_bpd.go
@@ -1,6 +1,7 @@
 package segment
 
 import (
+	"github.com/mitch000001/go-hbci/domain"
 	"github.com/mitch000001/go-hbci/element"
 )
 
@@ -8,6 +9,7 @@ const TanBankParameterID = "HITANS"
 
 type TanBankParameter interface {
 	BankSegment
+	TanProcessParameters() []domain.TanProcessParameter
 }
 
 //go:generate go run ../cmd/unmarshaler/unmarshaler_generator.go -segment TanBankParameterSegment -segment_interface TanBankParameter -segment_versions="TanBankParameterV6:6:Segment"
@@ -20,9 +22,15 @@ type TanBankParameterSegment struct {
 //
 // Zwei-Schritt-TAN-Einreichung, Parameter
 type TanBankParameterV6 struct {
-	Segment                     `yaml:"-"`
-	MaxJobs                     *element.NumberDataElement             `yaml:"MaxJobs"`
-	MinSignatures               *element.NumberDataElement             `yaml:"MinSignatures"`
+	Segment       `yaml:"-"`
+	MaxJobs       *element.NumberDataElement `yaml:"MaxJobs"`
+	MinSignatures *element.NumberDataElement `yaml:"MinSignatures"`
+	// Codierung:
+	// 0: kein Sicherheitsdienst erforderlich
+	// 1: Authentikation
+	// 2: Non-Repudiation mit fortgeschrittener elektronischer Signatur gemäß §2, SigG
+	// 3: Non-Repudiation mit fortgeschrittener elektronischer Signatur gemäß §2, SigG und zwingender Zertifikatsprüfung
+	// 4: Non-Repudiation mit qualifizierter elektronischer Signatur gemäß §2, SigG und zwingender Zertifikatsprüfung
 	SecurityClass               *element.CodeDataElement               `yaml:"SecurityClass"`
 	Tan2StepSubmissionParameter *element.Tan2StepSubmissionParameterV6 `yaml:"Tan2StepSubmissionParameter"`
 }
@@ -41,3 +49,22 @@ func (t *TanBankParameterV6) elements() []element.DataElement {
 	}
 }
 
+func (t *TanBankParameterV6) TanProcessParameters() []domain.TanProcessParameter {
+	var processParams []domain.TanProcessParameter
+	for _, de := range t.Tan2StepSubmissionParameter.ProcessParameters.GroupDataElements() {
+		pp, ok := de.(*element.Tan2StepSubmissionProcessParameterV6)
+		if !ok {
+			continue
+		}
+		processParams = append(processParams, domain.TanProcessParameter{
+			Steps:                 2,
+			SecurityFunction:      pp.SecurityFunction.Val(),
+			TanProcess:            pp.TanProcess.Val(),
+			TanProcessTechnicalID: pp.TechnicalIDTanProcess.Val(),
+			TanProcessName:        pp.ZKATanProcess.Val(),
+			TanProcessVersion:     pp.ZKATanProcessVersion.Val(),
+			TwoStepProcessName:    pp.TwoStepProcessName.Val(),
+		})
+	}
+	return processParams
+}
